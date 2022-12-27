145d9c2dcb05a8d490fb9890f47bc515668205bd
kol9ka <kol9ka@gmail.com>
2022-12-27 09:33:59 +0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const arr = ['a', 'b', 'c'];
+const arrObj = {
+    first: 'a',
+    second: 'b',
+    third: 'c'
+};
+
+arrObj.number = 1234;
+
+console.log(arrObj.number);
+console.log(arr[1]);
+console.log(arrObj.third);
+
+const obj = {
+    car: 'Ferrari',
+    model: '458',
+    year: 2004
+};
+
